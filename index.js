317974251a2b25ce7d01eeed63e700f209184bd7
唐宗超 <tangzongchao@meituan.com>
2019-03-26 17:10:06 +0800

    remove useless code

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const ratio = require('./ratio');
 const loan = require('./loan');
 
-console.log(loan._getFixedPrincipalDetail(
-  150 * 10000,
-  4.9 / 100 / 12,
-  25 * 12
-))
-
 module.exports = {
   getLoanDetail: loan.getLoanDetail,
   getSeriesRatioOfHousingFund: ratio.getSeriesRatioOfHousingFund,
